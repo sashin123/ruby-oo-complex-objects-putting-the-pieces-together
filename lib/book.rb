9a5985ee7680faf4c94267a13b41305ea9aac346
s.ttrout@hotmail.com <sashin123@LAPTOP-D36VJ7HR.localdomain>
2020-10-12 15:38:02 -0400

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,13 @@
+class Book
+    attr_accessor :author, :page_count, :genre
+    attr_reader :title
+  
+    def initialize(title)
+      @title = title
+    end
+    #Book.new("And Then There Were None")
 
-
+    def turn_page
+      puts "Flipping the page...wow, you read fast!"
+    end
+end
